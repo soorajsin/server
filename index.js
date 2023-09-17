db414524e2b3db333ea1df4d9ddcf482a156e82a
Sooraj Singh <115391013+soorajsin@users.noreply.github.com>
2023-09-17 16:37:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+          origin:["https://client-xi-flax.vercel.app"],
+          methods:["POST", "GET"],
+          credentials:true
+}));
 app.use(router);
 
 
